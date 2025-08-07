4f8b4d9acf595348206d379f0efbeefd5c3bc30b
luizaop11 <146454624+luizaop11@users.noreply.github.com>
2025-08-07 17:07:44 -0300

    l

---
+++ assets/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Curiosidade',
+    'Quem criou o termo "Inteligência Artificial" e quando?',
+    'Foi criado por John McCarthy em 1956, durante uma conferência no Dartmouth College. Ele é considerado o "pai da IA"'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a principal função das mitocôndrias nas células eucariontes?',
+    'As mitocôndrias são responsáveis pela produção de energia na forma de ATP, por meio da respiração celular.'
+)
+
+criaCartao(
+    'Português',
+    'Qual é a diferença entre DENOTAÇÃO e CONOTAÇÃO?',
+    'Denotação é o uso da palavra em seu sentido literal. Conotação é o uso da palavra no sentido figurado.'
+)
