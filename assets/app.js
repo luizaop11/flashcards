9f65337a3176c418e7acf5d16a10349b5f27b4e6
luizaop11 <146454624+luizaop11@users.noreply.github.com>
2025-08-07 17:47:03 -0300

    l

--- assets/app.js
+++ assets/app.js
@@ -5,12 +5,12 @@ function criaCartao(categoria, pergunta, resposta){
 
     cartao.innerHTML = `
     <div class="cartao__conteudo">
-                    <h3>Você sabia?</h3>
+                    <h3>${categoria}</h3>
                     <div class="cartao__conteudo__pergunta">
-                        <p>Qual animal tem três corações, sangue azul e pode desligar um braço para fugir?</p>
+                        <p>${pergunta}</p>
                     </div>
                     <div class="cartao__conteudo__resposta">
-                        <p>O polvo! Dois corações bombeiam sangue para as guelras e um para o corpo, seu sangue azul é por causa da hemocianina e solta um braço para escapar de predadores.</p>
+                        <p>${resposta}</p>
                     </div>
                 </div>
     `
